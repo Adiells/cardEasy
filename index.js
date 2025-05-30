eb88a8694c65fe8c222be1ff729b1eb4346d65c8
Adiel Emilson <adielenilson@gmail.com>
2025-05-30 00:23:45 -0300

    feat: implement session handling with login/logout and username-based dish route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
 const expressHandlebars = require('express-handlebars')
+const BetterSqlite3Store = require('better-sqlite3-session-store')(session)
 const multiparty = require('multiparty')
 const path = require('path')
 const db = require('./database/database')
 
 const handlers = require('./lib/handlers')
 const cadastro = require('./lib/cadastro')
+const { isAuthenticated } = require('./lib/middlewares/isAutenticated')
 const port = 3000
 
 const app = express()
@@ -19,9 +23,15 @@ const hbs = expressHandlebars.create({
         },
         lowerReplace: function(name){
             return name.toLowerCase().replace(/\s+/g, "")
+        },
+        ifEq: function(arg1, arg2, options) {
+            return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
         }
     }
 });
+
+const limpar = str => str.trimStart().length === 0 ? "" : str.trimStart();
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -30,7 +40,32 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session({
+    store: new BetterSqlite3Store({
+        client: db,
+        expired: {
+            clear: true,
+            interval: 900000
+        }
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 1000*60*60
+    }
+}))
+app.use((req, res, next) => {
+    if(req.session && req.session.user){
+        res.locals.user = req.session.user
+    }else{
+        res.locals.user = null
+    }
+    next()
+})
 
 app.get('/', handlers.home)
 
@@ -44,12 +79,31 @@ app.post('/api/cadastro', (req, res) => {
     if(!cadastro.isPasswordValid(password)) return res.status(500).json({err: 'sua senha não é usa senha válida'})
     if(!cadastro.isUsernameValid(username)) return res.status(500).json({err: 'seu username não é um username valido'})
     console.log(req.body)
-    if(name.length == 0 || name.length > 15) return res.status(500).json({err: 'seu nome é muito grande'})
+    if(limpar(name).length == 0 || name.length > 15) return res.status(500).json({err: 'seu nome é muito grande'})
     handlers.api.cadastro(req, res)
 })
 app.get('/api/usuarios/verificar', handlers.api.usersVerify)
-
-app.get('/restaurante/cadastro-pratos', handlers.cadastrarPratos)
+app.get('/login', handlers.login)
+app.post('/api/login', (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    if(limpar(username).length == 0 || username.length > 30){
+        return res.status(500).json({err: 'seu nome é muito grande ou vazio'})
+    }else if(limpar(password).length <= 3 || limpar(password).length > 30){
+        return res.status(500).json({err: 'seu nome é muito grande ou vazio'})
+    }
+    handlers.api.login(req, res)
+})
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Erro ao destruir a sessão:', err);
+            return res.redirect('/'); 
+        }
+        console.log('Sessão destruída.');
+        res.redirect('/'); 
+    });
+});
 app.get('/admin/deluser', (req, res) => {
     const stmt = db.prepare('SELECT * FROM users');
     let user = stmt.all()
@@ -66,6 +120,13 @@ app.post('/api/admin/deluser', (req, res) => {
     }
     res.status(201).json({teste: 'sim'})
 })
+app.get('/:username/cadastro-pratos', isAuthenticated, (req, res) => {
+    // if (req.session.user && req.session.user.username !== req.params.username) {
+    //     return res.status(403).send('Acesso negado.')
+    // }
+     handlers.cadastrarPratos(req, res)
+})
+
 app.post('/api/cadastro-pratos', (req, res) => {
     const form = new multiparty.Form()
     form.parse(req, (err, fields, files) => {
@@ -89,6 +150,11 @@ app.post('/api/cadastro-pratos', (req, res) => {
 app.get('/restaurantes', handlers.restaurantes)
 
 app.get('/restaurantes/:name', handlers.cardapio)
+
+app.use((req, res) => {
+  res.status(404).render('404', { titulo: 'Página não encontrada' });
+});
+
 
 if(require.main === module){
     app.listen(port, () => {
